bfb68fbe2c8889d19d8c2d3d41b88cf53ffef525
Anja Lečnik <anjas.lecnik@gmail.com>
2025-02-09 16:26:59 +0100

    [FIX] Added headers to loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,7 +24,15 @@ export const loader: LoaderFunction = async () => {
       {
         headers: {
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+          "Accept-Language": "en-US,en;q=0.9",
+          "Accept-Encoding": "gzip, deflate, br",
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+          Referer: "https://www.google.com/",
+          DNT: "1",
+          Connection: "keep-alive",
+          "Upgrade-Insecure-Requests": "1",
         },
       }
     );
